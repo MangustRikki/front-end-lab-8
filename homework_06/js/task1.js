87ff1de08f88459660333651d8c695132d757321
MangustRikki <mangust@yahoo.com>
2018-02-08 16:21:30 +0200

    done first task

--- homework_06/js/task1.js
+++ homework_06/js/task1.js
@@ -1,14 +1,33 @@
-let a = parseFloat(prompt("Введіть першу сторону трикутника", ""));
-let b = parseFloat(prompt("Введіть другу сторону трикутника", ""));
-let c = parseFloat(prompt("Введіть третю сторону трикутника", ""));
-let p = ((a + b + c) / 2);
+let a = parseFloat(prompt("Введіть першу сторону трикутника", "")),
+    b = parseFloat(prompt("Введіть другу сторону трикутника", "")),
+    c = parseFloat(prompt("Введіть третю сторону трикутника", "")),
+    p, 
+    sqr;
 
 if (!isNumeric(a) || !isNumeric(b) || !isNumeric(c)) {
     console.log("Incorrect data");
 } else if (a <= 0 || b <= 0 || c <= 0) {
     console.log("Incorrect data");
 } else {
-    console.log(Math.sqrt(p * (p - a) * (p - b) * (p - c)));
+    p = ((a + b + c) / 2);
+
+    var temporary = (p * (p - a) * (p - b) * (p - c));
+
+    if (temporary <= 0) {
+      console.log('Incorrect data');
+    }
+    else {
+    sqr = Math.sqrt(temporary).toFixed(2);
+      if (a * a == b * b + c * c || b * b == a * a + c * c || c * c == a * a + b * b) {
+      console.log('Type of triangle is Right triangle and square is ' + sqr);
+    } else if ((a == b) == c) {
+      console.log('Type of triangle is Equilateral triangle and square is ' + sqr);
+    } else if (a == b || a == c || b == c) {
+      console.log('Type of triangle is Isosceles triangle and square is ' + sqr);
+    } else {
+      console.log('Type of triangle is Scalene triangle and square is ' + sqr);
+    }
+    }
 }
 
 
