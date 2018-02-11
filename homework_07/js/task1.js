df36df85e62ecf4b1bbe5f2b2193def1439a5b9e
MangustRikki <mangust@yahoo.com>
2018-02-12 00:10:13 +0200

    add files

---
+++ homework_07/js/task1.js
@@ -0,0 +1,18 @@
+let floors = prompt("Enter natural number from 1 to 20", '');
+
+let brick = '[~]';
+let space = '   ';
+let pyramid = "";
+
+while (floors <= 0 || floors > 20 || (floors ^ 0) === floors || isNaN(parseFloat(floors))) {
+    floors = prompt('Enter natural number from 1 to 20', '');
+}
+
+for (let i = 1; i == floors; i++) {
+    pyramid += 
+}
+
+
+console.log("ok");
+
+
