8bd3b3b9f1032c5cd91d876e934b63cf750749b9
MangustRikki <mangust@yahoo.com>
2018-02-12 15:58:32 +0200

    second task done

--- homework_07/js/task2.js
+++ homework_07/js/task2.js
@@ -1,6 +1,9 @@
-let answer = confirm("Do you want to play a game?");
+let newGame = confirm("Do you want to play a game?");
 
-let game = {
+let game = {};
+
+while (newGame) {
+    game = {
         min: 0,
         max: 5,
         prize: 10,
@@ -8,39 +11,38 @@ let game = {
         secretNum: "",
         attempts: 3,
         winPrize: 0,
-};
-
-
-
-while (answer) {
+    };
     game.secretNum = Math.floor(Math.random() * (game.max - game.min)) + game.min;
+    console.log(game.secretNum);
     let i = 1; // to divide price for each attempts
-    while (game.attempts > 0) {
-        
-        game.guess = prompt(` Enter a number from ${game.min} to ${game.max} \n Attempts left: ${game.attempts} \n Total prize: ${game.winPrize} \n Possibly prize on current attempt: ${parseInt(game.prize/i)}`, "");        
 
-    if (game.guess == game.secretNum) {
-        let contGame = confirm("Do you want to continue the game?"); 
-            if(contGame) {
+    while (game.attempts > 0) {
+
+        game.guess = prompt(` Enter a number from ${game.min} to ${game.max} \n Attempts left: ${game.attempts} \n Total prize: ${game.winPrize}$ \n Possibly prize on current attempt: ${game.prize}$`, "");
+
+        if (game.guess == game.secretNum) {
+            let contGame = confirm("Do you want to continue the game?");
+            if (contGame) {
                 game.attempts = 3;
                 game.winPrize += game.prize;
                 game.max *= 2;
                 game.prize *= 3;
 
                 game.secretNum = Math.floor(Math.random() * (game.max - game.min)) + game.min;
+            } else {
+                game.winPrize += game.prize;
+                console.log(`Thank you for a game. Your prize is: ${game.winPrize}$`);
+
+                newGame = confirm("Do you want to play again?");
             }
 
-            else {
-                
-            }
-
-        
-
-        
+        } else {
+            game.attempts -= 1;
+            game.prize = parseInt(game.prize / i);
+        }
+        i = 2; //to divide prize
     }
-
-
-}
+    newGame = confirm("Do you want to play again?");
 
 }
 
