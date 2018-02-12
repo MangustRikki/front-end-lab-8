af03dc0f4fa86edc50f39abf3c1ac62e1cfa2aeb
MangustRikki <mangust@yahoo.com>
2018-02-12 12:39:16 +0200

    first task done

--- homework_07/js/task1.js
+++ homework_07/js/task1.js
@@ -1,18 +1,25 @@
 let floors = prompt("Enter natural number from 1 to 20", '');
 
-let brick = '[~]';
-let space = '   ';
 let pyramid = "";
+let i = 0;
 
 while (floors <= 0 || floors > 20 || (floors ^ 0) === floors || isNaN(parseFloat(floors))) {
     floors = prompt('Enter natural number from 1 to 20', '');
 }
 
-for (let i = 1; i == floors; i++) {
-        for(let k = 0; k < floors; k++) {
-            pyramid += space;
-        }
-} 
+while (i < floors) {
+    let brick = "";
+    let space = "";
+ 
+    for (let k = 1; k < floors - i; k++) {
+        space += "   ";
+    }
+    for (let j = 0; j < 2 * i +1; j++) {
+        brick += '[~]';
+    }
+    pyramid += space + brick + '\n';
+    i++;
+}
 
-
+console.log(pyramid);
 
