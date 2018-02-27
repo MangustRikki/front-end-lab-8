bf17dfb9b87d5544df26278a287b2e60aa8b086e
MangustRikki <mangust@yahoo.com>
2018-02-27 23:43:08 +0200

    add func to make folder open and close

--- homework_11/app.js
+++ homework_11/app.js
@@ -4,45 +4,44 @@ let rootNode = document.getElementById("root");
 let folderList = document.createElement('ul');
 let item = document.createElement('li');
 let folderImg = document.createElement('i');
-    folderImg.classList.add('material-icons', 'md-dark');
-    folderImg.textContent = "folder";
+folderImg.classList.add('material-icons', 'md-dark');
+folderImg.textContent = "folder";
 
 let fileImg = makeClone(folderImg, false);
 
-    fileImg.classList.add('gray');
-    fileImg.textContent = "insert_drive_file";
+fileImg.classList.add('gray');
+fileImg.textContent = "insert_drive_file";
 
 
 function createTree(parent, child, array) {
     array.forEach((item) => {
 
         let cloneChild = makeClone(child, false);
-            cloneChild.textContent = item.title;
+        cloneChild.textContent = item.title;
         let cloneParent = makeClone(parent, false);
-            cloneParent.setAttribute('hidden', 'true');
-            
+        cloneParent.setAttribute('hidden', 'true');
 
-        if(item.folder) { 
+
+        if (item.folder) {
             cloneChild.prepend(makeClone(folderImg, true));
             cloneChild.classList.add('folder');
             if (item.children) {
-            createTree(cloneParent, child, item.children);
-            cloneChild.appendChild(cloneParent);
-            }
-            else {
+                createTree(cloneParent, child, item.children);
+                cloneChild.appendChild(cloneParent);
+            } else {
 
                 let emptyFolder = document.createElement('p');
-                    emptyFolder.setAttribute('hidden', 'true');
-                    emptyFolder.textContent = "Folder is empty";
+                emptyFolder.setAttribute('hidden', 'true');
+                emptyFolder.textContent = "Folder is empty";
                 cloneChild.appendChild(emptyFolder);
             }
         } else {
             cloneChild.prepend(makeClone(fileImg, true));
         }
 
-            parent.appendChild(cloneChild);
-        });
-        
+        parent.appendChild(cloneChild);
+    });
+
     return parent;
 }
 
@@ -58,43 +57,46 @@ rootNode.appendChild(allList);
 
 function showHide(e) {
     let target = e.target.closest('.folder').children[1];
-    console.log(e.target);
-    if(target) {
+    let targetImg = e.target.closest('.folder').children[0];
+    console.log(targetImg);
+    if (target) {
 
-    let isHidden = target.getAttribute('hidden');
-    let isFolder = e.target.classList.contains('folder');
-        console.log(isFolder);
-    if (isHidden) {
-      target.removeAttribute('hidden');
-    } else if (isFolder ) {
-      target.setAttribute('hidden', 'true');
-    } 
+        let isHidden = target.getAttribute('hidden');
+        let isFolder = e.target.classList.contains('folder');
+        
+        if (isHidden) {
+            target.removeAttribute('hidden');
+            targetImg.textContent = 'folder_open';
+        } else if (isFolder) {
+            target.setAttribute('hidden', 'true');
+            targetImg.textContent = 'folder';
+        }
     }
 
-//     let target = this.lastElementChild;
-//     let target2 = this.firstElementChild;
-//     console.log(this);
-//     if (target) {    
-// //    console.log(target);
-//     if(target.getAttribute('hidden') === 'true') {
-       
-//         target.removeAttribute('hidden');
-       
-//         if (this.classList.contains('folder')) {
-//             this.classList.add('open');
-//         }
-       
-//     }
-//      else if (this.classList.contains('open')) {
-//          console.log('lalal');
-//         this.lastElementChild.setAttribute('hidden', 'true');
-//         this.classList.remove('open');
-//      }
-        
-//     }
+    //     let target = this.lastElementChild;
+    //     let target2 = this.firstElementChild;
+    //     console.log(this);
+    //     if (target) {    
+    // //    console.log(target);
+    //     if(target.getAttribute('hidden') === 'true') {
 
-//     if (target2.className === "material-icons md-dark") {
-//         target2.textContent = "folder_open";
-//     }
+    //         target.removeAttribute('hidden');
+
+    //         if (this.classList.contains('folder')) {
+    //             this.classList.add('open');
+    //         }
+
+    //     }
+    //      else if (this.classList.contains('open')) {
+    //          console.log('lalal');
+    //         this.lastElementChild.setAttribute('hidden', 'true');
+    //         this.classList.remove('open');
+    //      }
+
+    //     }
+
+    //     if (target2.className === "material-icons md-dark") {
+    //         target2.textContent = "folder_open";
+    //     }
 
 }
