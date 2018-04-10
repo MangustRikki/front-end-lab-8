34f692c1f27bc5b4c61b17e90aa3ab268b246ba8
Bogdan Mylash <mangust@yahoo.com>
2018-04-10 23:56:13 +0300

    update

--- homework_08/server.js
+++ homework_08/server.js
@@ -3,7 +3,7 @@ const express = require('express'),
     bodyParser = require('body-parser'),
     app = express(),
     routes = require('./routes'),
-    port = process.env.PORT || 3000;
+    port = process.env.PORT || 3050;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
